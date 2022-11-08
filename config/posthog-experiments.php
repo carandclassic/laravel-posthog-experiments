af6d52b105fbce47b70eecfd3db4c8dd8d5efefe
Wesley Martin <wesley.martin@carandclassic.com>
2022-11-08 11:55:07 +0200

    Adding config file options

--- config/posthog-experiments.php
+++ config/posthog-experiments.php
@@ -1,6 +1,26 @@
 <?php
 
-// config for CarAndClassic/PosthogExperiments
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | PostHog API Key
+    |--------------------------------------------------------------------------
+    |
+    | This value is the API key that is supplied to you from PostHog.
+    | You should be able to find this under the Project Seettings
+    | https://app.posthog.com/project/settings
+    |
+    */
+    'key' => env('POSTHOG_API_KEY', ''),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Application Name
+    |--------------------------------------------------------------------------
+    |
+    | This value is the fallback unique identifier
+    | That is used when a user is not logged in.
+    |
+    */
+    'cookie_key' => env('POSTHOG_COOKIE_KEY', 'laravel_session'),
 ];
