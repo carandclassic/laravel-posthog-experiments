f0e3102931d6aa4101ef85c9c0f51ce1986786d5
Wesley Martin <wesley.martin@carandclassic.com>
2022-11-08 10:34:19 +0200

    Move to using PHPUnit

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,5 +1,16 @@
 <?php
 
-it('can test', function () {
-    expect(true)->toBeTrue();
-});
+declare(strict_types=1);
+
+namespace CarAndClassic\PosthogExperiments\Tests;
+
+use CarAndClassic\PosthogExperiments\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testItCanTest(): void
+    {
+        $this->assertTrue(true);
+    }
+}
+
