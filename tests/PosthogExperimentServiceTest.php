ab564811c5f13cfcd20e1755271031e795bc8dd3
Wesley Martin <wesley.martin@carandclassic.com>
2022-11-09 11:01:47 +0200

    Adding PosthogExperimentService test

---
+++ tests/PosthogExperimentServiceTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CarAndClassic\PosthogExperiments\Tests;
+
+use CarAndClassic\PosthogExperiments\Services\PosthogExperimentService;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
+
+class ExampleTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Cache::flush();
+        Http::fake([
+            '*' => Http::response([
+                "featureFlags" => [
+                    "sell-your-classic-test" => "input_cta_change"
+                ],
+            ], 200),
+        ]);
+    }
+
+    public function testItCanGetAFeatureFlag(): void
+    {
+        $experiment = 'test';
+        $participant = '1';
+
+        $featureFlag = PosthogExperimentService::getFeatureFlag($experiment, $participant);
+
+        $this->assertSame('input_cta_change', $featureFlag);
+        $this->assertTrue(Cache::has($experiment . md5($participant)));
+    }
+}
