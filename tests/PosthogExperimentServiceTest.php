e6c8974926c2ece3739764b4d3ef389f317625da
Wesley Martin <wesley.martin@carandclassic.com>
2022-11-10 15:26:47 +0200

    Adding more tests

--- tests/PosthogExperimentServiceTest.php
+++ tests/PosthogExperimentServiceTest.php
@@ -4,17 +4,21 @@ declare(strict_types=1);
 
 namespace CarAndClassic\PosthogExperiments\Tests;
 
+use CarAndClassic\PosthogExperiments\Jobs\SendFeatureFlagCalledJob;
 use CarAndClassic\PosthogExperiments\PosthogExperimentsService;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Queue;
 
-class PosthogExperimentTest extends TestCase
+class PosthogExperimentServiceTest extends TestCase
 {
     public function setUp(): void
     {
         parent::setUp();
 
         Cache::flush();
+        Queue::fake();
         Http::fake([
             '*' => Http::response([
                 'featureFlags' => [
@@ -33,6 +37,20 @@ class PosthogExperimentTest extends TestCase
 
         $this->assertSame('test_a', $featureFlag);
         $this->assertTrue(Cache::has($experiment . md5($participant)));
+        Queue::after(function (SendFeatureFlagCalledJob $event) {
+            $this->assertTrue($event->job->isReleased());
+        });
+    }
+
+    public function testItHandlesAnIncorrectExperimentPassedIn(): void
+    {
+        $experiment = 'wrong-test';
+        $participant = '1';
+
+        $featureFlag = PosthogExperimentsService::getFeatureFlag($experiment, $participant);
+
+        $this->assertSame('', $featureFlag);
+        $this->assertFalse(Cache::has($experiment . md5($participant)));
     }
 
     public function testItCanTestIfSpecificFeatureFlagsAreSetByPassingInAnArray(): void
